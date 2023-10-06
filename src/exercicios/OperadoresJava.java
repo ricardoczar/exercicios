9ac56a96f297ca20562bff7f0e57c4a372ec0c94
Ricardo Cézar <91541813+ricardoczar@users.noreply.github.com>
2023-10-05 22:58:27 -0300

    aula 40 a 45

---
+++ src/exercicios/OperadoresJava.java
@@ -0,0 +1,24 @@
+package exercicios;
+
+public class OperadoresJava {
+
+    public static void main(String[] args) {
+
+        var x = 34.50;
+        double y = 20;
+
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+
+        int a =12;
+        int b =10;
+
+        System.out.println(x+(double)y);
+        System.out.println(x%y);
+
+
+
+    }
+}
